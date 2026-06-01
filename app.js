5ae0a9d4508ce42666b3f9c0534d70bf99d45207
learning git <d.chandanadamisetti@gmail.com>
2026-06-01 14:02:24 -0500

    new message

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 
+const PORT = 3000;
+
+// Route
 app.get("/", (req, res) => {
-  res.send("Hello from Jenkins pipeline 🚀");
+  res.send(`
+    Hey Teja 😌💖  
+    Just letting you know my heart is currently running on Teja.exe and it refuses to shut down 😏💻  
+
+    Warning: side effects include smiling for no reason and missing you randomly ❤️
+  `);
 });
 
-app.listen(3000, () => {
-  console.log("App running on port 3000");
+// Start server
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port 3000`);
 });
-
-
